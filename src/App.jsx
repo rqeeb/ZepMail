ce869c502168df9b872d2b79b8a7ec970c40fcac
RaqeebDev <raqeebauto@gmail.com>
2026-02-17 14:25:22 +0530

    function for random strings

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { useEffect } from "react";
 
 import "./App.css";
 
+//Function for Random strings (Return random mail!)
+function genLocalMail() {
+  const user = Math.random().toString(36).slice(2, 10);
+  return "{user}@mails.zepmail.xyz";
+}
+
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <div>Hey!</div>
-  );
+  return <div>Hey!</div>;
 }
 
 export default App;
