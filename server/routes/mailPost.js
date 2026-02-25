396d4404d0d70159b761a35df9fd7f18c2686a1d
RaqeebDev <raqeebauto@gmail.com>
2026-02-25 21:32:19 +0530

    back end bug'

--- server/routes/mailPost.js
+++ server/routes/mailPost.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const app = express();
-const mailPost = express.Router();
-const mailModel = require("../db");
+// const express = require("express");
+// const app = express();
+// const mailPost = express.Router();
+// const mailModel = require("../db");
 
-// mailPost.post("/post", (req, res) => {
-//   const email = req.email;
-//   const time = req.time;
+//  mailPost.post("/post", (req, res) => {
+//  const email = req.email;
+//  const time = req.time;
   
-//   try {
+//  try {
 //     await mailModel.create({
 //       email:email,
-//       time:time,
-//     })
+//      time:time,
+//    })
 //     .then(() => {
 //       console.log("111");
 //     })
 //   }
 //   catch(err){
-//     console.log(`Error: ${err}`);
-//   }
+//      console.log(`Error: ${err}`);
+//    }
 
  
 
-  //make mongo , send to db , delete if pressed refresh/after 40 mins,  
-})
+//   //make mongo , send to db , delete if pressed refresh/after 40 mins,  
+// })
 
 // module.exports = {
-//   mailPost: mailPost,
+//    mailPost: mailPost,
 // };
